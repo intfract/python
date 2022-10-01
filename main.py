9a484aa627daa94bd011d9457b47543d560ef1b2
Int Fract <110207136+intfract@users.noreply.github.com>
2022-10-01 20:12:40 +0000

    added a lot of github api requests

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import math
 import time
 import requests
 import json
+import os
+
+def r(n, to):
+  return round(n / to) * to
 
 def _(string, data):
   return string.format(**data)
@@ -10,11 +14,41 @@ def get(url):
   res = requests.get(url)
   return json.loads(res.content.decode('utf-8'))
 
+def repo(author, repo):
+  data = get(_('https://api.github.com/repos/{author}/{repo}', {
+    'repo': repo,
+    'author': author,
+  }))
+
+  owner = data['owner']['login']
+  
+  langs = get(_('https://api.github.com/repos/{author}/{repo}/languages', {
+    'repo': repo,
+    'author': author,
+  }))
+  
+  ratio = {}
+  total = 0
+  
+  for lang in langs.keys():
+    total += langs[lang]
+  
+  for lang in langs.keys():
+    ratio[lang] = r((langs[lang] / total) * 100, 1)
+  
+  return {
+    'owner': owner,
+    'languages': ratio,
+  }
+
+username = 'intfract'
+
 user = get(_('https://api.github.com/users/{username}', { 
-  'username': 'intfract',
+  'username': username,
 }))
 
 users = get('https://api.github.com/search/users?q=type%3Auser')
 
 print(user['bio'])
 print(users['total_count'])
+print(repo('intfract', 'defract'))
